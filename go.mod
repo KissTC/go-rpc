d93430177885e48c11af14f79dde3b313225f583
Julio Paredes <ing.julio.code@gmail.com>
2023-06-27 19:32:28 -0600

    the RPC function to get twitter profile from a json file

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go-RPC
 
 go 1.18
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/rpc v1.2.0 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -1,14 +1,65 @@
 package main
 
 import (
+	jsonparse "encoding/json"
+	"io/ioutil"
 	"log"
-	"net"
 	"net/http"
-	"net/rpc"
-	"time"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/rpc"
+	"github.com/gorilla/rpc/json"
 )
 
-type Args struct{}
+// this holds the arguments passed to json rpc service
+type Args struct {
+	Name string
+}
+
+// twitterprofile holds
+type TwitterProfile struct {
+	Name      string `json:"name,omitempty"`
+	Username  string `json:"username,omitempty"`
+	Followers string `json:"followers,omitempty"`
+	Following string `json:"following,omitempty"`
+}
+
+type JSONServer struct{}
+
+//twitterprofile detail
+func (t *JSONServer) TwitterProfileDetail(r *http.Request, args *Args, reply *TwitterProfile) error {
+	var twitterProfiles []TwitterProfile
+	// Read JSON file and load data
+	raw, err := ioutil.ReadFile("./twitterProfile.json")
+	if err != nil {
+		log.Fatal("error: ", err)
+		os.Exit(1)
+	}
+
+	//Unmarshall json raw data into twitter profiles array
+	marshallerr := jsonparse.Unmarshal(raw, &twitterProfiles)
+	if marshallerr != nil {
+		log.Println("error:", marshallerr)
+		os.Exit(1)
+	}
+
+	// Iterate over each twitter profile to find the given twitter profile
+	for _, twitterProfile := range twitterProfiles {
+		if twitterProfile.Name == args.Name {
+			*reply = twitterProfile
+			break
+		}
+	}
+
+	return nil
+
+}
+
+/*
+
+the RPC time server legacy
+
 type TimeServer int64
 
 func (t *TimeServer) GiveServerTime(args *Args, reply *int64) error {
@@ -16,17 +67,17 @@ func (t *TimeServer) GiveServerTime(args *Args, reply *int64) error {
 	*reply = time.Now().Unix()
 	return nil
 }
+*/
 
 func main() {
 	// create a new RPC server
-	timeserver := new(TimeServer)
-	// Register RPC server
-	rpc.Register(timeserver)
-	rpc.HandleHTTP()
-	// listen for request on port 1234
-	l, e := net.Listen("tcp", ":2233")
-	if e != nil {
-		log.Fatal("listen error:", e)
-	}
-	http.Serve(l, nil)
+	// timeserver := new(TimeServer) no library
+	// Create a new RPC server
+	s := rpc.NewServer() // Register the type of data requested as JSON
+	s.RegisterCodec(json.NewCodec(), "application/json")
+	// Register the service by creating a new JSON server
+	s.RegisterService(new(JSONServer), "")
+	r := mux.NewRouter()
+	r.Handle("/rpc", s)
+	http.ListenAndServe(":9000", r)
 }
